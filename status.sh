e87eb83cd259c043d69430939ad61114adcf4225
Travis Turk <55367187+turkosaurus@users.noreply.github.com>
2025-02-10 15:41:56 -0700

    remove unused/replaced status checker

--- status.sh
+++ status.sh
@@ -1,17 +1,3 @@
-#!
-echo 'running status.sh'
+#!/use/bin/env bash
 
-sites=(
-    'https://www.turkosaur.us'
-    'https://nailivic.herokuapp.com/login'
-    'https://fake.zzzz'
-)
-for site in ${sites[*]}
-do
-  response=$(curl -LIs $site -o /dev/null -w '%{http_code}')
-  echo $(date +"%Y-%m-%dT%H:%M:%S%:z")',' $response',' $site >> status.csv
-  if [ "$response" != 200 ]; then
-    echo ERROR:$site #TODO have this trigger an alert
-  fi
-done
-cat status.csv
+echo "deprecated but not removed (I'm on a phone rn)”
